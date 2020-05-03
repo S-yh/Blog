f15fc517cea9eb95a91be050fb8dc0044c5d9904
石宇航 <260287@gree.com.cn>
2020-05-03 15:06:43 +0800

    init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,30 @@
+module.exports = {
+    title: '随笔一记',
+    base: '/blog/',
+    description: "石宇航的博客",
+    dest: public,
+    head: [
+        ['link', {rel: 'icon', href: './public/favicon.ico'}]
+    ],
+    themeConfig: {
+        nav:[
+            { text: 'Home', link: '/'},
+            { text: 'GitHub', link: 'https://github.com/S-yh/blog'}
+        ],
+        sidebar: [
+            ['/html/', 'HTML'],
+            ['/css/', 'CSS'],
+            ['/js/', 'JavaScript'],
+            ['/vue/', 'Vue'],
+            ['/react/', 'React'],
+            ['/node/', 'Node'],
+            ['/git/', 'Git'],
+            ['/webpack/', 'Webpack'],
+            ['/docker/', 'Docker'],
+            ['/browser/', '浏览器'],
+            ['/snippet/', '工具函数/代码片段'],
+            ['/algorithm/', '数据结构/算法']
+        ],
+        sidebarDepth: 1
+    }
+}
